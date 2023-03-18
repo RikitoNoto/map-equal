3fb9d5cfa6aa9d85c747ef3a626156f9b4ec354b
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-03-18 14:24:22 +0900

    create mapEqual func and create compare size procces

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,3 +1,18 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1).toBe(1);
+import { mapEqual } from "..";
+
+describe('Test: mapEqual', ()=>{
+
+  describe('Map<int, int>', ()=>{
+    it('should be return true, if the func called with the same empty maps', () => {
+      expect(mapEqual(new Map<number, number>(), new Map<number, number>())).toBe(true);
+    });
+  
+    it('should be return false, if the func called with the different size maps', () => {
+      expect(mapEqual(new Map<number, number>(), new Map<number, number>([[1,1]]))).toBe(false);
+    });
+  
+    it('should be return false, if the func called with the same size but different content maps', () => {
+      expect(mapEqual(new Map<number, number>([[2,2]]), new Map<number, number>([[1,1]]))).toBe(false);
+    });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-function a(): void{
-  ;
+export function mapEqual<T, E>(map1: Map<T, E>, map2: Map<T, E>): boolean{
+  if(map1.size !== map2.size) return false;
+  
+  console.log(map1.size);
+  for (const key1 of map1.keys()) {
+    for (const key2 of map2.keys()) {
+      if(key1 !== key2) return false;
+    }
+  }
+
+  return true;
 }
