336185d14faf64fc195138853418cef89d77eb55
R.Noto <rikitonoto@gmail.com>
2023-03-18 21:43:43 +0900

    create int map compare method

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,16 +3,19 @@ import { mapEqual } from "..";
 describe('Test: mapEqual', ()=>{
 
   describe('Map<int, int>', ()=>{
-    it('should be return true, if the func called with the same empty maps', () => {
-      expect(mapEqual(new Map<number, number>(), new Map<number, number>())).toBe(true);
+    it.each([
+      {map1: new Map<number, number>(), map2: new Map<number, number>(), result: true},
+      {map1: new Map<number, number>(), map2: new Map<number, number>([[1,1]]), result: false},
+      {map1: new Map<number, number>([[2,2]]), map2: new Map<number, number>([[1,1]]), result: false},
+      {map1: new Map<number, number>([[2,2]]), map2: new Map<number, number>([[2,2]]), result: true},
+      {map1: new Map<number, number>([[2,2]]), map2: new Map<number, number>([[2,2]]), result: true},
+      {map1: new Map<number, number>([[2,1]]), map2: new Map<number, number>([[2,2]]), result: false},
+      {map1: new Map<number, number>([[3,2], [6,9]]), map2: new Map<number, number>([[3,2], [4,5]]), result: false},
+      {map1: new Map<number, number>([[3,2], [6,9]]), map2: new Map<number, number>([[3,2], [6,9]]), result: true},
+    ])("should be return $result when contents are ($map1Content) ($map2Content).",
+    ({map1, map2, result})=>{
+      expect(mapEqual(map1, map2)).toBe(result);
     });
-  
-    it('should be return false, if the func called with the different size maps', () => {
-      expect(mapEqual(new Map<number, number>(), new Map<number, number>([[1,1]]))).toBe(false);
-    });
-  
-    it('should be return false, if the func called with the same size but different content maps', () => {
-      expect(mapEqual(new Map<number, number>([[2,2]]), new Map<number, number>([[1,1]]))).toBe(false);
-    });
+
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 export function mapEqual<T, E>(map1: Map<T, E>, map2: Map<T, E>): boolean{
   if(map1.size !== map2.size) return false;
-  
-  console.log(map1.size);
+
   for (const key1 of map1.keys()) {
+    let hasSameKey = false;
     for (const key2 of map2.keys()) {
-      if(key1 !== key2) return false;
+      if(key1 === key2){
+        hasSameKey = true;
+        if(map1.get(key1) !== map2.get(key2)) return false;
+        break;
+      }
     }
+
+    if(!hasSameKey) return false;
   }
 
   return true;
